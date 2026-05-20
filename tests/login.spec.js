f4562eb2ff213b6caef3156dd79a073a37d0c668
Karna1919 <saikumarkandrathi7@gmail.com>
2026-05-20 13:21:17 +0530

    Provided Locater

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ import {test, expect} from '@playwright/test';
 test("login test",async ({page})=>{
 
     await page.goto("/TestServer/Build/Advance_Library_System/");
-    await page.locator("//a[@href='index.php?module=Calendar&action=index']").click();
+    await page.locator("//a[href='home.php']").click();
 });
 
 
